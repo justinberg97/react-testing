4a9eb31c9c01956aabdb2e4e743787a3e4c4c0f2
Justin Berg <justin.berg1@verizon.com>
2025-02-25 14:04:25 -0600

    finished assignment

--- src/App.test.js
+++ src/App.test.js
@@ -9,44 +9,52 @@ import { App } from './App';
  */
 test('App should render', () => {
   render(<App />);
-
   expect(screen.getByText('Welcome, party people!')).toBeInTheDocument();
 });
 
+/**
+ * Verify button should render
+ */
 test('Button should render', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  expect(screen.getByText('Current theme: light')).toBeInTheDocument();
 });
 
 /**
  * Verify clicking button should change theme
- * hint: use fireEvent.click(element) to trigger a click event on an element
  */
 test('theme button should update button text', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const themeButton = screen.getByText('Current theme: light');
+  fireEvent.click(themeButton);
+  expect(screen.getByText('Current theme: dark')).toBeInTheDocument();
 });
 
-// BONUS
-// hint: there is a `.toHaveStyle` method.
-// e.g.: expect(element).toHaveStyle('color: #FFF');
+/**
+ * Verify theme button should toggle styles
+ */
 test('theme button should toggle styles', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const themeButton = screen.getByText('Current theme: light');
+  fireEvent.click(themeButton);
+  expect(document.body).toHaveStyle('background-color: #333');
+  expect(document.body).toHaveStyle('color: #FFF');
+  fireEvent.click(themeButton);
+  expect(document.body).toHaveStyle('background-color: #FFF');
+  expect(document.body).toHaveStyle('color: #333');
 });
 
 /**
  * Verify clicking button should toggle hidden content
- *
- * hint: you can check if something does not exist by using .not
- * e.g. expect(element).not.toBeInTheDocument()
- *
- * hint: use `queryByText` instead of `getByText` to check if something is _not_ rendered
- * (getByText will throw an error if it is not rendered)
  */
 test('hidden button should toggle hidden content', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const toggleButton = screen.getByText('Show hidden content');
+  expect(screen.queryByText('this content is hidden by default')).not.toBeInTheDocument();
+  fireEvent.click(toggleButton);
+  expect(screen.getByText('this content is hidden by default')).toBeInTheDocument();
+  fireEvent.click(toggleButton);
+  expect(screen.queryByText('this content is hidden by default')).not.toBeInTheDocument();
 });
 
 
